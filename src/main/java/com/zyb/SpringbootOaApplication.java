332580d9263c2655077a9df07fc49d8864d349f6
zyb <1774418005@qq.com>
2019-12-27 10:13:04 +0800

    创建项目

---
+++ src/main/java/com/zyb/SpringbootOaApplication.java
@@ -0,0 +1,13 @@
+package com.zyb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOaApplication.class, args);
+    }
+
+}
